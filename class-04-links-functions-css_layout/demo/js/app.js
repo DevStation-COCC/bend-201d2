502200ec726b0794ca9c61194bf12d7d353ea04b
DukeOfEtiquette <duquetteadam@gmail.com>
2019-03-14 12:20:43 -0700

    Add comments to class-04 demo

--- class-04-links-functions-css_layout/demo/js/app.js
+++ class-04-links-functions-css_layout/demo/js/app.js
@@ -1,3 +1,4 @@
+// Globally scoped variable, usable in all functions delcared in this file
 var myName = 'Adam';
 
 function greeter() {
@@ -10,31 +11,39 @@ greeter();
 
 var otherGreeter = function(name) {
   var message = 'Hello ' + name;
-  return message;
+  return message; // Returns the VALUE of the variable 'message'
 };
 
 
+// otherGreeter returns a String
 var myGreeting = otherGreeter('Ellie');
 console.log(myGreeting); // expected output: 'Hello Ellie'
 
+// otherGreeter returns a String which is passed as an argument to console.log
 console.log(otherGreeter('Evelyn')); // output: 'Hello Evelyn'
 
 var bar = function(){
   console.log('Hello from bar!');
 };
 
+// cb is common shorthand for 'callback'
+// https://developer.mozilla.org/en-US/docs/Glossary/Callback_function 
 function foo(cb) {
   cb();
 }
 
+// The FUNCTION is the argument, we do not invoke!
 foo(bar);
 
-var message = (function(){
-  return 'Hello from an IIFE!';
-})();
+// Surround the function expression with parentheses to "bundle" the function, then add two more parentheses to the end to INVOKE the function (you can pass arguments into this set of parentheses)
+var message = (function(name){
+  return 'Hello from an ' + name + '!';
+})('IIFE');
 
+// The above IIFE returns a string value, so we print via console.log
 console.log(message);
 
+// Flag variable
 var isPingTurn = true;
 
 var pingpong = function(){
@@ -43,10 +52,13 @@ var pingpong = function(){
   }else{
     console.log('pong');
   }
+
+  // Use the ! symbol (logical NOT operator) to toggle the boolean value
   isPingTurn = !isPingTurn;
 };
 
 var pingpongIntervalId = setInterval(pingpong, 1000);
 
 // Comment out to turn on Ping Pong
+// clearInterval stops the callback function from being invoked further
 clearInterval(pingpongIntervalId, 5000);
